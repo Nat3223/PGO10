2895811903a51bff4f2e5a6fd32b6773c285ba4f
Natalia Suwińska <s33171@pjwstk.edu.pl>
2026-05-12 09:05:07 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Task 1");
+
         UserValidator validator = new UserValidator();
 
         validator.addRule(form -> form.email().contains("@"));
@@ -15,5 +17,30 @@ public class Main {
         );
 
         System.out.println("Is the form valid? " + validator.isValid(form));
+
+        System.out.println("\n Task 2");
+
+        ServiceOrder order = new ServiceOrder(
+                "Alpha Company",
+                10,
+                120.0
+        );
+
+        PriceCalculator calculator = new PriceCalculator();
+
+        PriceStrategy standard =orderData -> orderData.hours() * orderData.hourRate();
+
+        PriceStrategy discount = orderData -> orderData.hours() * orderData.hourRate() * 0.90;
+
+        PriceStrategy weekend = orderData -> orderData.hours() * orderData.hourRate() * 1.25;
+
+        System.out.println("Standard price: " + calculator.calculate(order, standard)
+        );
+
+        System.out.println("Discount price: " + calculator.calculate(order, discount)
+        );
+
+        System.out.println("Weekend price: " + calculator.calculate(order, weekend)
+        );
     }
 }
